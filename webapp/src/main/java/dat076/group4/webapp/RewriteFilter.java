7cdb507ef2384af972aa4430fac9042fee4d068a
Magnus <M92@users.noreply.github.com>
2014-10-27 12:07:50 +0100

    Solved the exclusion problem in the rewrite filter

---
+++ webapp/src/main/java/dat076/group4/webapp/RewriteFilter.java
@@ -0,0 +1,31 @@
+package dat076.group4.webapp;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
+
+/**
+ * Custom URL rewrite filter that can exclude
+ * paths from Tuckey's UrlRewriteFilter.
+ */
+public class RewriteFilter extends UrlRewriteFilter {
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+            throws IOException, ServletException {
+
+        String path = ((HttpServletRequest)request).getRequestURI();
+
+        if (path.startsWith("/webapp/api")) {
+            // Continue as normal, do not rewrite
+            chain.doFilter(request, response);
+        } else {
+            // Process the request in UrlRewriteFilter
+            super.doFilter(request, response, chain);
+        }
+    }
+}
